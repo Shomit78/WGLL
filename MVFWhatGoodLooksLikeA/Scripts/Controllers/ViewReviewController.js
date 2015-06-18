28f2154a6804fc9b2076793922190486032698c1
shomit78@hotmail.com <shomit78@hotmail.com>
2015-06-18 12:03:03 +0100

    Pulling back review details on view review.html

--- MVFWhatGoodLooksLikeA/Scripts/Controllers/ViewReviewController.js
+++ MVFWhatGoodLooksLikeA/Scripts/Controllers/ViewReviewController.js
@@ -2,6 +2,30 @@
     SP.SOD.executeOrDelayUntilScriptLoaded(ViewReviewControllerOnLoad, "SP.js");
     function ViewReviewControllerOnLoad() {
 
+        var currentReviewId = $routeParams.ReviewId;
+        var filterByReviewId = "Title eq '" + currentReviewId + "'";
+        $scope.reviews = [];
+
+        $.when(SharePointJSOMService.getItemsFromHostWebWithParams($scope, 'Reviews', 'Title,ID,WGLLStatus,WGLLStore,WGLLVisitType,WGLLVisitSummary,WGLLNotes', '', filterByReviewId, ''))
+        .done(function (jsonObject) {
+            angular.forEach(jsonObject.d.results, function (review) {
+                $scope.reviews.push({
+                    id: review.ID,
+                    title: review.Title,
+                    status: review.WGLLStatus,
+                    store: review.WGLLStore,
+                    visitType: review.WGLLVisitType,
+                    visitSummary: review.WGLLVisitSummary,
+                    notes: review.WGLLNotes
+                });
+                console.log(review.ID + ";" + review.Title);
+                if (!$scope.$$phase) { $scope.$apply(); }
+            });
+        })
+        .fail(function (err) {
+            console.info(JSON.stringify(err));
+        });
+
         $scope.goTo = function (path) {
             $location.path(path);
         };
