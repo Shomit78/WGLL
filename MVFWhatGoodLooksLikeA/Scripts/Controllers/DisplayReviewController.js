4614c47e28cdfc871348446b8db015dc1a3a0508
shomit78@hotmail.com <shomit78@hotmail.com>
2015-07-08 09:59:12 +0100

    Images displayed on DisplayReview.html

--- MVFWhatGoodLooksLikeA/Scripts/Controllers/DisplayReviewController.js
+++ MVFWhatGoodLooksLikeA/Scripts/Controllers/DisplayReviewController.js
@@ -20,71 +20,98 @@
                     $scope.visitType = review.WGLLVisitType;
                     $scope.visitSummary = review.WGLLVisitSummary;
                 });
-                var ans = [];
-                var subfilter = "(" + sharePointConfig.fields.answers.reviewId + " eq '" + $scope.title + "')";
-                //Get the answers related to the Review from the Answers lists
-                $.when(SharePointJSOMService.getItemsFromHostWebWithParams($scope, sharePointConfig.lists.answers,
-                    sharePointConfig.fields.sharepoint.title + ',' + sharePointConfig.fields.sharepoint.id +
-                    ',' + sharePointConfig.fields.answers.detail + ',' + sharePointConfig.fields.answers.subset +
-                    ',' + sharePointConfig.fields.answers.result + ',' + sharePointConfig.fields.answers.reasonForFailure +
-                    ',' + sharePointConfig.fields.answers.nonNegotiable, '',
-                    subfilter, sharePointConfig.fields.answers.order))
-                   .done(function (jsonObject) {
-                       angular.forEach(jsonObject.d.results, function (answer) {
-                           ans.push({
-                               title: answer.Title,
-                               id: answer.ID,
-                               detail: answer.WGLLCriteriaDetail,
-                               subset: answer.WGLLSubset,
-                               result: answer.WGLLResult,
-                               reasonForFailure: answer.WGLLReasonForFailure,
-                               nonNegotiable: answer.WGLLNonNegotiable
-                           });
-                       });
-                       var filter = sharePointConfig.fields.subsets.active + " eq 1";
-                       //Get a list of active Subsets from the Subset list to display with Answers
-                       $.when(SharePointJSOMService.getItemsFromHostWebWithParams($scope, sharePointConfig.lists.subsets,
-                           sharePointConfig.fields.sharepoint.title + ',' + sharePointConfig.fields.sharepoint.id + ',' +
-                           sharePointConfig.fields.subsets.detail + ',' + sharePointConfig.fields.subsets.order, '',
-                           filter, sharePointConfig.fields.subsets.order))
-                            .done(function (jsonObject) {
-                                angular.forEach(jsonObject.d.results, function (subset) {
-                                    var subsetAnswers = [];
-                                    angular.forEach(ans, function (criteria) {
-                                        if (criteria.subset == subset.Title) {
-                                            subsetAnswers.push({
-                                                title: criteria.title,
-                                                id: criteria.id,
-                                                detail: criteria.detail,
-                                                subset: criteria.subset,
-                                                result: criteria.result,
-                                                reasonForFailure: criteria.reasonForFailure,
-                                                nonNegotiable: criteria.nonNegotiable
+                var images = [];
+                $.when(SharePointJSOMService.getImagesFromHostWebFolder($scope,
+                         "/mvf/wgll/" + sharePointConfig.lists.images + "/" + $scope.store + "/" + $scope.title))
+                    .done(function (jsonObject) {
+                        angular.forEach(jsonObject.d.results, function (image) {
+                            images.push({
+                                name: image.Name,
+                                serverRelativeUrl: image.ServerRelativeUrl,
+                                answerId: image.ListItemAllFields.WGLLAnswerId
+                            });
+                        });
+                        var ans = [];
+                        var subfilter = "(" + sharePointConfig.fields.answers.reviewId + " eq '" + $scope.title + "')";
+                        //Get the answers related to the Review from the Answers lists
+                        $.when(SharePointJSOMService.getItemsFromHostWebWithParams($scope, sharePointConfig.lists.answers,
+                            sharePointConfig.fields.sharepoint.title + ',' + sharePointConfig.fields.sharepoint.id +
+                            ',' + sharePointConfig.fields.answers.detail + ',' + sharePointConfig.fields.answers.subset +
+                            ',' + sharePointConfig.fields.answers.result + ',' + sharePointConfig.fields.answers.reasonForFailure +
+                            ',' + sharePointConfig.fields.answers.nonNegotiable, '',
+                            subfilter, sharePointConfig.fields.answers.order))
+                           .done(function (jsonObject) {
+                               angular.forEach(jsonObject.d.results, function (answer) {
+                                   ans.push({
+                                       title: answer.Title,
+                                       id: answer.ID,
+                                       detail: answer.WGLLCriteriaDetail,
+                                       subset: answer.WGLLSubset,
+                                       result: answer.WGLLResult,
+                                       reasonForFailure: answer.WGLLReasonForFailure,
+                                       nonNegotiable: answer.WGLLNonNegotiable
+                                   });
+                               });
+                               var filter = sharePointConfig.fields.subsets.active + " eq 1";
+                               //Get a list of active Subsets from the Subset list to display with Answers
+                               $.when(SharePointJSOMService.getItemsFromHostWebWithParams($scope, sharePointConfig.lists.subsets,
+                                   sharePointConfig.fields.sharepoint.title + ',' + sharePointConfig.fields.sharepoint.id + ',' +
+                                   sharePointConfig.fields.subsets.detail + ',' + sharePointConfig.fields.subsets.order, '',
+                                   filter, sharePointConfig.fields.subsets.order))
+                                    .done(function (jsonObject) {
+                                        angular.forEach(jsonObject.d.results, function (subset) {
+                                            var subsetAnswers = [];
+                                            angular.forEach(ans, function (criteria) {
+                                                var answerImages = [];
+                                                angular.forEach(images, function (img) {
+                                                    if (img.answerId == criteria.id) {
+                                                        answerImages.push({
+                                                            name: img.name,
+                                                            serverRelativeUrl: img.serverRelativeUrl,
+                                                            answerId: img.answerId
+                                                        });
+                                                    }
+                                                });
+                                                if (criteria.subset == subset.Title) {
+                                                    subsetAnswers.push({
+                                                        title: criteria.title,
+                                                        id: criteria.id,
+                                                        detail: criteria.detail,
+                                                        subset: criteria.subset,
+                                                        result: criteria.result,
+                                                        reasonForFailure: criteria.reasonForFailure,
+                                                        nonNegotiable: criteria.nonNegotiable,
+                                                        images: answerImages
+                                                    });
+                                                }
                                             });
-                                        }
-                                    });
-                                    $scope.subsets.push({
-                                        title: subset.Title,
-                                        id: subset.ID,
-                                        order: subset.SubsetOrder,
-                                        detail: subset.SubsetDetail,
-                                        answers: subsetAnswers
-                                    });
-                                });
-                                //$scope is not updating so force with this command
-                                if (!$scope.$$phase) { $scope.$apply(); }
-                            })
+                                            $scope.subsets.push({
+                                                title: subset.Title,
+                                                id: subset.ID,
+                                                order: subset.SubsetOrder,
+                                                detail: subset.SubsetDetail,
+                                                answers: subsetAnswers
+                                            });
+                                        });
+                                        //$scope is not updating so force with this command
+                                        if (!$scope.$$phase) { $scope.$apply(); }
+                                    })
+                                   .fail(function (err) {
+                                       SP.UI.Notify.addNotification(sharePointConfig.messages.defaultError, false);
+                                       console.info(JSON.stringify(err));
+                                   });
+                               //$scope is not updating so force with this command
+                               if (!$scope.$$phase) { $scope.$apply(); }
+                           })
                            .fail(function (err) {
                                SP.UI.Notify.addNotification(sharePointConfig.messages.defaultError, false);
                                console.info(JSON.stringify(err));
                            });
-                       //$scope is not updating so force with this command
-                       if (!$scope.$$phase) { $scope.$apply(); }
-                   })
-                   .fail(function (err) {
-                       SP.UI.Notify.addNotification(sharePointConfig.messages.defaultError, false);
-                       console.info(JSON.stringify(err));
-                   });
+                    })
+                    .fail(function (jsonObject) {
+                        SP.UI.Notify.addNotification(sharePointConfig.messages.defaultError, false);
+                        console.info(JSON.stringify(err));
+                    });
                 //$scope is not updating so force with this command
                 if (!$scope.$$phase) { $scope.$apply(); }
             })
